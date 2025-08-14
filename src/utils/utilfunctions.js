2275ee459b8a5fa7d3c637ce69aed794a28e8223
Avinash <avinashkmg1310@gmail.com>
2025-08-14 16:42:57 +0530

    +Sugyan : utils function to add classNames dynamically for styling Result section

--- src/utils/utilfunctions.js
+++ src/utils/utilfunctions.js
@@ -1,27 +1,33 @@
-export default function WeightCat(result){
+export default function WeightCat(result) {
+	let weightCat;
+	let cname;
 
-    let weightCat;
+	switch (true) {
+		case result < 16:
+			weightCat = 0;
+			cname = "underweight";
+			break;
+		case result < 18.5 && result >= 16:
+			weightCat = 1;
+			cname = "underweight";
+			break;
+		case result >= 18.5 && result < 25:
+			weightCat = 2;
+			cname = "normal";
+			break;
+		case result >= 25.0 && result < 30:
+			weightCat = 3;
+			cname = "overweight";
+			break;
+		case result >= 30 && result < 35:
+			weightCat = 4;
+			cname = "obese-1";
+			break;
+		case result >= 35:
+			weightCat = 5;
+			cname = "obese-2";
+			break;
+	}
 
-    switch (true){
-        case result<16 : 
-            weightCat= 0;
-            break;
-        case result<18.5 && result >=16: 
-            weightCat= 1;
-            break;
-        case result>=18.5 && result<25 :
-             weightCat=2;
-             break;
-        case result>=25.0 && result<30 :
-            weightCat=3;
-            break;
-        case result>=30 && result<35 :
-            weightCat=4;
-            break;
-        case result>=35 :
-            weightCat=5;
-            break;
-    }
-
-    return weightCat;
+	return { weightCat, cname };
 }
